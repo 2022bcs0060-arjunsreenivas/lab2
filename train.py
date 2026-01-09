454edb1f4053f5680d5fb96aaf7dd96fd1399428
arjunmedath2003 <arjunrains2003@gmail.com>
2026-01-09 12:22:07 +0530

    Ridge Regression

--- train.py
+++ train.py
@@ -4,17 +4,19 @@ import joblib
 import json
 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
 
 data = pd.read_csv("dataset/winequality-white.csv",sep=";")
 X = data.drop(['quality'],axis=1)
 y = data['quality']
-X_subset = X.drop(["alcohol", "sulphates"], axis=1)
+scalar = StandardScaler()
+X_scaled = scalar.fit_transform(X)
 
-X_train,X_test,y_train,y_test = train_test_split(X_subset,y,test_size=0.2, random_state=1234)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2, random_state=1234)
 
-model = LinearRegression()
+model = Ridge()
 model.fit(X_train,y_train)
 
 pred = model.predict(X_test)
