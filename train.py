396eaf5fb710e3a80479771e251a5d3aa2d3f7eb
arjunmedath2003 <arjunrains2003@gmail.com>
2026-01-09 12:28:47 +0530

    Random Forest

--- train.py
+++ train.py
@@ -6,17 +6,16 @@ import json
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 data = pd.read_csv("dataset/winequality-white.csv",sep=";")
 X = data.drop(['quality'],axis=1)
 y = data['quality']
-scalar = StandardScaler()
-X_scaled = scalar.fit_transform(X)
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2, random_state=1234)
 
-model = Ridge()
+model = RandomForestRegressor(random_state=42)
 model.fit(X_train,y_train)
 
 pred = model.predict(X_test)
